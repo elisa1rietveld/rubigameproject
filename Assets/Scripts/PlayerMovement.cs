20cf1ae099e8e1dd4f56820d27c3c7e1b5a4703c
Marvin <142317017+stmmarvin@users.noreply.github.com>
2025-02-10 11:51:07 +0100

    Player movement gemaakt met goedkeuring van Elisa

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        rb.velocity = new Vector2 (moveHorizontal * speed, moveVertical * speed);
+
+
+        Vector3 pos = transform.position;
+        Vector3 screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+
+
+        pos.x = Mathf.Clamp(pos.x, -screenBounds.x, screenBounds.x);
+        pos.y = Mathf.Clamp(pos.y, -screenBounds.y, screenBounds.y);
+        
+        transform.position = pos;
+    }
+}
