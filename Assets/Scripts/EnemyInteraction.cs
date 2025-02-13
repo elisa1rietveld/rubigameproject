17a872504238026a06afebe56288c1f6653743bf
RensLisman <150025637+RensLisman@users.noreply.github.com>
2025-02-13 20:53:33 +0100

    Rens-EnemyHacking-1
    enemy's are hackable and some of the animations added

---
+++ Assets/Scripts/EnemyInteraction.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using UnityEngine.UI;  
+using System.Collections;
+
+public class EnemyInteraction : MonoBehaviour
+{
+    public Slider slider;              
+    public Sprite badEnemySprite;      
+    public Sprite changedEnemySprite; 
+    private SpriteRenderer enemyRenderer;  
+    private bool isInteracting = false;
+
+    
+    public float interactionRange = 2f;  
+    private bool playerInRange = false;  
+
+    void Start()
+    {
+        enemyRenderer = GetComponent<SpriteRenderer>();
+        
+        if (enemyRenderer == null)
+        {
+            Debug.LogError("No SpriteRenderer found on the enemy GameObject!");
+            return;
+        }
+        
+        enemyRenderer.sprite = badEnemySprite;  
+    }
+
+    void Update()
+    {
+        if (playerInRange && !isInteracting)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                StartCoroutine(StartSliderInteraction());
+            }
+        }
+    }
+
+    IEnumerator StartSliderInteraction()
+    {
+        slider.gameObject.SetActive(true); 
+        slider.value = 0;
+
+      
+        isInteracting = true;
+
+        float timePassed = 0;
+        while (timePassed < 5f)
+        {
+            timePassed += Time.deltaTime;
+            slider.value = timePassed / 5f; 
+
+            yield return null;  
+        }
+
+        enemyRenderer.sprite = changedEnemySprite;
+        slider.gameObject.SetActive(false);
+        isInteracting = false;
+    }
+
+   
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))  
+        {
+            playerInRange = true;  
+        }
+    }
+
+    
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))  
+        {
+            playerInRange = false;  
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,28 +6,50 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     private Rigidbody2D rb;
-     
-    // Start is called before the first frame update
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-        rb.velocity = new Vector2 (moveHorizontal * speed, moveVertical * speed);
 
+        rb.velocity = new Vector2(moveHorizontal * speed, moveVertical * speed);
+
+        float movementSpeed = new Vector2(moveHorizontal, moveVertical).magnitude;
+        animator.SetFloat("Speed", movementSpeed);
+
+        if (moveHorizontal > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (moveHorizontal < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetBool("IsJumping", true);
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            animator.SetBool("IsJumping", false);
+        }
 
         Vector3 pos = transform.position;
         Vector3 screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
 
-
         pos.x = Mathf.Clamp(pos.x, -screenBounds.x, screenBounds.x);
         pos.y = Mathf.Clamp(pos.y, -screenBounds.y, screenBounds.y);
-        
+
         transform.position = pos;
     }
 }
+
