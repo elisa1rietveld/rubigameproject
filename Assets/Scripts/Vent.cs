4f0f172339630570b610e7b71896710cc1252d39
Marvin <142317017+stmmarvin@users.noreply.github.com>
2025-03-16 12:39:59 +0100

    Player vent nu in de zelde scene naar de andere sprite

--- Assets/Scripts/Vent.cs
+++ Assets/Scripts/Vent.cs
@@ -1,48 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-
 
 public class Vent : MonoBehaviour
 {
-    public GameObject teleportation;
+    public GameObject teleportation; // Het lege gameobject waar de speler naartoe wordt geteleporteerd
+    public Transform teleport; // De positie waar de speler naartoe wordt geteleporteerd
+    private Animator ventAnimator; // Referentie naar de Animator component
+    public GameObject VentLocation;
     public float openDistance = 2.5f;
-    public Transform teleport;
+    public Camera mainCamera;
+    public Camera secondCamera;
     private GameObject player;
-    private Animator ventAnimator;
+
     // Start is called before the first frame update
     void Start()
     {
+
+        ventAnimator = GetComponentInChildren<Animator>();
         player = GameObject.FindGameObjectWithTag("Player");
-        ventAnimator = GetComponent<Animator>();
 
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (player != null)
         {
+            // Bereken de afstand tussen de vent en de speler
             float distance = Vector2.Distance(transform.position, player.transform.position);
 
+            // Als de speler dichtbij genoeg is, open de vent
             if (distance <= openDistance)
             {
-                ventAnimator.SetBool("isOpen", true);
-            }
-            else 
-            { 
-              ventAnimator.SetBool("isOpen", false);
-            }
-        }
-    }
+                Debug.Log("Player is within open distance. Opening vent.");
+                ventAnimator.SetBool("IsOpen", true);
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            player.transform.position = teleport.position;
-            SceneManager.LoadScene(1);
+                // Controleer of de speler op de K-toets drukt om te teleporteren
+                if (Input.GetKeyDown(KeyCode.K))
+                {
+                    Debug.Log("K key pressed.");
+                    // Teleporteer de speler naar de teleport positie
+                    if (VentLocation != null)
+                    {
+                        Debug.Log("Teleporting player to VentLocation.");
+                        player.transform.position = VentLocation.transform.position;
+
+                        // Schakel de camera's
+                        if (mainCamera != null && secondCamera != null)
+                        {
+                            mainCamera.enabled = false;
+                            secondCamera.enabled = true;
+                        }
+                    }
+                    else
+                    {
+                        Debug.LogWarning("VentLocation is not assigned.");
+                    }
+                }
+            }
+            else
+            {
+                // Als de speler te ver weg is, sluit de vent
+                Debug.Log("Player is too far. Closing vent.");
+                ventAnimator.SetBool("IsOpen", false);
+            }
         }
     }
 }
